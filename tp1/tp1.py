f5e5e5811ce185b036a3845149c23d120d63d452
Franco Navarro <franconavarro1889@gmail.com>
2022-03-25 22:34:48 -0300

    comienzo de correcciones

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -3,23 +3,11 @@ import matplotlib.pyplot as plt
 import statistics as st
 
 
-"""def promedio(n):
-    prome = []
-    for i in range(n):
-        prome.append(np.random.randint(0, 36))
-    return sum(prome)/len(prome)"""
-
-
 def carga_lista(n, valor_max):
-    #lista = []
-    #for i in range(n):
-    #    lista.append(np.random.randint(0, 36))
-    #return lista
     return np.random.randint(0, valor_max + 1, size=n)
 
 
 def frecuencia_rel(x, valor_max):
-    # freqs = [(value, x.count(value)/len(x)) for value in set(x)]
     # x es un array de NumPy, comparar tira un array de booleanos (equivalente a 0s y 1s), la suma
     # da la cantidad
     return [(x == value).sum()/len(x) for value in range(0, valor_max + 1)]
@@ -50,39 +38,61 @@ def carga_list_desvio(lista):
 
 
 def main():
-    prom, varianza, desvio = [], [], []
+    "Se declaran las listas variables"
+    lista, prom, varianza, desvio = [], [], [], []
+
+    "Se declaran las listas a devolver"
+    listas, promedios, varianzas, desvios, frecuencias = [], [], [], [], []
+    n = 100
     var_esperado = 120
     desv_esperado = 10
     frec_esperado = 0.04
     prom_esperado = 15
 
 
-    n = 100
-    valor_max = 36
-    lista = carga_lista(n, valor_max)
-    prom = carga_list_prom(lista)
-    varianza = carga_list_varianza(lista)
-    desvio = carga_list_desvio(lista)
-    freqs = frecuencia_rel(lista, valor_max)
-    print(f'La lista es: {lista}')
-    print(f'El promedio es {prom}')
-    print(f'La varianza es {varianza}')
-    print(f'El desvío es {desvio}')
-    print(freqs)
+
+    for i in range(10):
+        valor_max = 36
+        lista = carga_lista(n, valor_max)
+        prom = carga_list_prom(lista)
+        varianza = carga_list_varianza(lista)
+        desvio = carga_list_desvio(lista)
+        freqs = frecuencia_rel(lista, valor_max)
+        print(f'La lista es: {lista}')
+        print(f'El promedio es {prom}')
+        print(f'La varianza es {varianza}')
+        print(f'El desvío es {desvio}')
+        print(freqs)
+
+
+
+        promedios.extend(prom)
+        listas.extend(lista)
+        varianzas.extend(varianza)
+        desvios.extend(desvio)
+        frecuencias.extend(freqs)
+
+    "Imágen de puntos"
+    fig, lis = plt.subplots()
+    lis.scatter(range(1000), listas, s=2)
+    lis.set_title('Números:', loc='left',
+                  fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
+    lis.set_xlabel('Tirada')
+    lis.set_ylabel('Número')
 
     "Imágen de varianza"
     fig, var = plt.subplots()
-    var.plot(range(n), varianza)
-    var.plot([0, n], [var_esperado, var_esperado], marker='o', linestyle='dotted')
-    var.set_title('Varianza:', loc='left',
-                 fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
+    var.plot(range(1000), varianzas)
+    var.plot([0, 1000], [var_esperado, var_esperado], marker='o', linestyle='dotted')
+    var.set_title('Varianzas:', loc='left',
+                  fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
     var.set_xlabel('Tirada')
     var.set_ylabel('Variación')
 
     "Imágen de frecuencias"
     fig, frec = plt.subplots()
-    frec.bar(range(len(freqs)), freqs)
-    frec.plot([0, n], [frec_esperado, frec_esperado], marker='o', linestyle='dotted')
+    frec.bar(range(len(frecuencias)), frecuencias)
+    frec.plot([0, 1000], [frec_esperado, frec_esperado], marker='o', linestyle='dotted')
     frec.set_title('Frecuencias relativas:', loc='left',
                    fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
     frec.set_xlabel('Número')
@@ -90,30 +100,23 @@ def main():
 
     "Imágen de desvíos"
     fig, des = plt.subplots()
-    des.plot(range(n), desvio)
-    des.plot([0, n], [desv_esperado, desv_esperado], marker='|', linestyle='dashed')
-    des.set_title('Desvío estándar:', loc='left',
-                 fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
+    des.plot(range(1000), desvios)
+    des.plot([0, 1000], [desv_esperado, desv_esperado], marker='|', linestyle='dashed')
+    des.set_title('Desvíos:', loc='left',
+                  fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
     des.set_xlabel('Tirada')
     des.set_ylabel('Desvío Estándar')
 
     "Comienzo de creación de imágen de promedio"
     fig, ax = plt.subplots()
-    ax.plot(range(n), prom)
-
-    # Aquí se grafica la recta del promedio de las tiradas realizadas
-    ax.plot([0, n], [prom_esperado, prom_esperado], marker='o', linestyle='dotted')
-
-    # Se configuran algunos títulos para mejorar la vista del gráfico
-    ax.set_title('Promedio de tiradas:', loc='left', fontdict={'fontsize':14, 'fontweight':'bold', 'color':'tab:blue'})
+    ax.plot(range(1000), promedios)
+    ax.plot([0, 1000], [prom_esperado, prom_esperado], marker='o', linestyle='dotted')
+    ax.set_title('Promedio de tiradas:', loc='left',
+                 fontdict={'fontsize': 14, 'fontweight': 'bold', 'color': 'tab:blue'})
     ax.set_xlabel('Tirada')
     ax.set_ylabel('Promedio')
 
-    ""
-
     plt.show()
-
-    # frecuencia relativa, varianza, desvío y promedio
 
 
 if __name__ == '__main__':
