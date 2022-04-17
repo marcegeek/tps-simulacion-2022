65fc4840f9ce3e6c652257e69f442b3fb02092b8
Marcelo Castellano <marce.geek22@gmail.com>
2022-04-17 19:32:52 -0300

    Implementar clase abstracta del generador y algunas mejoras

--- tp3/tp3.py
+++ tp3/tp3.py
@@ -1,4 +1,45 @@
-class GeneradorGCL:
+import abc
+
+
+class Generador(abc.ABC):
+    """
+    Clase base abstracta general de un generador de números pseudoaleatorios
+    """
+
+    def random(self, size=None):
+        """
+        Devuelve un flotante aleatorio en el intervalo semiabierto [0, 1).
+        O una lista si se especifica el parámetro size.
+        """
+        def func():
+            return self._rand()/(self._rand_max + 1)
+        if size is None:
+            return func()
+        return [func() for _ in range(size)]
+
+    def randint(self, a, b, size=None):
+        """
+        Devuelve un entero aleatorio en el rango [a, b], incluyendo los extremos.
+        O una lista si se especifica el parámetro size.
+        """
+        def func():
+            # código basado en http://c-faq.com/lib/randrange.html
+            return a + self._rand() // (self._rand_max // (b - a + 1) + 1)
+        if size is None:
+            return func()
+        return [func() for _ in range(size)]
+
+    @abc.abstractmethod
+    def _rand(self):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def _rand_max(self):
+        pass
+
+
+class GeneradorGCL(Generador):
     """
     Implementación general de un generador congruencial lineal
     """
@@ -8,17 +49,14 @@ class GeneradorGCL:
         self.multiplicador = multiplicador
         self.incremento = incremento
         self.valor = semilla
-        self._max = modulo - 1
 
-    def _next_raw_int(self):
+    def _rand(self):
         self.valor = (self.multiplicador * self.valor + self.incremento) % self.modulo
         return self.valor
 
-    def randint(self, a, b):
-        return a + int(self.random() * (b - a) + 0.5)
-
-    def random(self):
-        return self._next_raw_int()/self._max
+    @property
+    def _rand_max(self):
+        return self.modulo - 1
 
 
 class GCLAnsiC(GeneradorGCL):
@@ -42,13 +80,19 @@ class GCLRandu(GeneradorGCL):
 
 
 def main():
-    generador = GCLAnsiC(10)
-    nums = []
-    for i in range(100):
-        nums.append(generador.randint(0, 1))
-    print(nums)
-    print(nums.count(0)/len(nums))
-    print(nums.count(1)/len(nums))
+    semilla = 10
+
+    generador = GCLAnsiC(semilla)
+    nums_size = generador.randint(0, 1, size=100)
+
+    generador = GCLAnsiC(semilla)
+    nums_list = []
+    for i in range(len(nums_size)):
+        nums_list.append(generador.randint(0, 1))
+    print(nums_size)
+    print(nums_size.count(0)/len(nums_size))
+    print(nums_size.count(1)/len(nums_size))
+    assert nums_size == nums_list
 
 
 if __name__ == '__main__':
