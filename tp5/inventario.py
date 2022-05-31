f019baf74b648c52df721a839f4c4aa0a7e89b7d
Franco Navarro <franconavarro1889@gmail.com>
2022-05-30 23:06:18 -0300

    cambio de modo de calcular el tiempo de evento

--- tp5/inventario.py
+++ tp5/inventario.py
@@ -16,7 +16,6 @@ class ModeloInventario(Simulacion):
         self.nivel_inventario = 60
         self.mantenidos_area = 0
         self.area_escacez = 0
-        self.orden_arribos(8)
         self.bigs = 40
         self.smalls = 20
         self.costo_incremental = 3
@@ -26,20 +25,20 @@ class ModeloInventario(Simulacion):
         self.costo_reserva = 5
 
         self.costo_mantenimiento = 1
-        self.tasa_arribos = tasa_arribos ##ver si esto se puede poner en la clase Simulacion
+        self.mediana_entredemanda = 0.1
 
     def orden_arribos(self, cantidad, siguiente):
         self.nivel_inventario += cantidad
         self.costo_total_ordenado += cantidad * self.costo_incremental
         self.eventos.append(
-            Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.tasa_arribos), siguiente)
+            Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.mediana_entredemanda), siguiente)
             #El self.siguiente lo agregué porque no sabría que metodo podría agregar al init de Evento
         )
 
     def demanda(self, cant_demand, siguiente):
         self.nivel_inventario -= cant_demand
         self.eventos.append(
-            Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.tasa_arribos), siguiente)
+            Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.mediana_entredemanda), siguiente)
         )
 
     def evaluar(self, siguiente):
@@ -47,13 +46,14 @@ class ModeloInventario(Simulacion):
             cantidad = self.bigs - self.nivel_inventario
             self.costo_total_ordenado = self.setup_cost + self.costo_incremental * cantidad
             self.eventos.append(
-                Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.tasa_arribos), siguiente)
+                Evento(self.reloj + self.np_randomstate.exponential(scale=1 / self.mediana_entredemanda), siguiente)
             )
 
     def reporte(self):
         costo_ordenes_prom = self.costo_total_ordenado / self.meses
         costo_mantenimiento_prom = self.costo_mantenimiento * self.mantenidos_area / self.meses
         costo_escacez_prom = self.costo_reserva * self.area_escacez / self.meses
+        costo_total = costo_ordenes_prom + costo_mantenimiento_prom + costo_escacez_prom
 
     def actualizar_estadisticas(self):
         super().actualizar_estadisticas()
